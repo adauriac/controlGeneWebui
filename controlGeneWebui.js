afc52cf593100affc3f31b0da8b4f0f669cd8907
U-DESKTOP-5JA619C\ACXYS <jeanchristian@anglesdauriac.fr>
2023-12-14 17:45:07 +0100

    precedent sans inutilerie

--- controlGeneWebui.js
+++ controlGeneWebui.js
@@ -1,9 +1,9 @@
 console.log("hi 2")
-//let elt = document.getElementById("plasma3")
-//console.log(elt)
-//console.log(elt.style)
-//elt.style.background = "red"
+let elt = document.getElementById("plasma3")
+console.log(elt)
+console.log(elt.style)
+elt.style.background = "red"
 
-let elt = document.getElementById("powerMeasure")
+elt = document.getElementById("powerMeasure")
 console.log(elt.innerHTML)
-elt.innerHTML = "fromjs"
+elt.innerHTML = "Set fromjs"
